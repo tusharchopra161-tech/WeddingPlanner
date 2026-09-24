08041959ba12c0101610f835f28696a381d3e388
tusharchopra161-tech <tusharchopra161@gmail.com>
2026-09-24 20:53:58 +0530

    2_commit

--- server.js
+++ server.js
@@ -2,17 +2,8 @@ const express=require("express")
 const app=express()
 const dotenv=require("dotenv");
 dotenv.config()
-const port=process.env.PORT
 app.use(express.json())
 const connectDB=require("./config/db")
-// app.use("/users",userRoutes)
-// app.use("/products",productsRoutes)
-// app.use("/orders",orderRoutes)
-// app.use(errHandleMiddleware)
-app.get("/",(req,res)=>{
-    res.send("hey this is this is you")
-})
-app.listen(port,async()=>{
-    await connectDB()
-    console.log("server listen at 3000")
-})
+ connectDB()
+    
+module.exports=app
